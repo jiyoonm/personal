b680b911cafde7c21029f5593c2a4ea072aa0efe
jiyoonm <54877411+jiyoonm@users.noreply.github.com>
2023-07-07 20:13:02 -0500

    add sample data

---
+++ src/dummy-data/archive-grid-info.ts
@@ -0,0 +1,47 @@
+import sampleImg from "../../public/static/bg11.jpeg";
+const archiveInfo = [
+  {
+    imgUrl: sampleImg,
+    title: "1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "6",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    imgUrl: sampleImg,
+    title: "7",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+];
+
+export default archiveInfo;
