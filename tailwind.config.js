c006e44ff1d175dcd316da06b22b1ee44931fca7
jiyoonm <54877411+jiyoonm@users.noreply.github.com>
2023-07-07 20:12:40 -0500

    project configuration:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-light": "#9A998B",
+        primary: "#EEEEEE",
+        secondary: "#2C2C2C",
+        "red-accent": "#EC6969",
       },
     },
   },
   plugins: [],
-}
+};
